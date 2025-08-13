23681d4887173bdf1d443cd544f268fb6e051cc9
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-13 23:29:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -492,7 +492,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '',
-            image:'https://i.ibb.co/pBy3vKYY/MEF-AAP-027-BAP-2025-08-13-20-22-32.png',
+            image:'https://i.ibb.co/7N23CPws/MEF-AAP-027-BAP-2025-08-13-20-28-16.png',
             description: '.',
             location: {
                 center: [28.981, 41.027],
